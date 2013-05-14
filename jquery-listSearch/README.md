c7b41907edb743809908d683444b6221a46a32bf
David Cabrera <david@rerainc.com>
2013-05-14 18:05:34 -0400

    change ulsearch to listsearch

---
+++ jquery-listSearch/README.md
@@ -0,0 +1,11 @@
+jQuery listSearch
+===============
+
+This is a tiny jQuery plugin which prepends and wires an instant-search box to html list(s). 
+
+[View it live](http://wpb.org/city-services).
+[GitHub Project](https://github.com/rera/jquery-listsearch).
+
+### Stack
+* JQUERY
+* JAVASCRIPT
--- jquery-listSearch/jquery.listsearch.js
+++ jquery-listSearch/jquery.listsearch.js
@@ -15,14 +15,14 @@
         }
     };
 
-    $.fn.ulSearch = function(methodOrOptions) {
+    $.fn.listSearch = function(methodOrOptions) {
         if ( methods[methodOrOptions] ) {
             return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
         } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
             // Default to "init"
             return methods.init.apply( this, arguments );
         } else {
-            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.ulSearch' );
+            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.listSearch' );
         }    
     };
 })( jQuery );
