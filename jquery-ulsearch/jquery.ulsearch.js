7dfa5823b8cadbd691f4799ad7794d8c9d6db778
David Cabrera <david@rerainc.com>
2013-05-14 18:29:46 -0300

    Update jquery.ulsearch.js

--- jquery-ulsearch/jquery.ulsearch.js
+++ jquery-ulsearch/jquery.ulsearch.js
@@ -1,7 +1,7 @@
 (function( $ ){
-    var methods = {
-        init : function(options) {
-        	var search = $('<input type="text" name="search"' + ( (options.hasOwnProperty('searchclass') && typeof options.searchclass === 'string') ? ' class="'+options.searchclass+'"' : '' ) + ' placeholder="Search" value="" />');
+	var methods = {
+		init : function(options) {
+			var search = $('<input type="text" name="search"' + ( (options.hasOwnProperty('searchclass') && typeof options.searchclass === 'string') ? ' class="'+options.searchclass+'"' : '' ) + ' placeholder="Search" value="" />');
 			$(search).insertBefore(this);
 			$(search).change(function() {
 				$(this).next().find('li').css("display", "none").each(function() {
@@ -12,17 +12,17 @@
 			}).keyup(function() {
 				$(this).change();
 			});	
-        }
-    };
-
-    $.fn.ulSearch = function(methodOrOptions) {
-        if ( methods[methodOrOptions] ) {
-            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
-            // Default to "init"
-            return methods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.ulSearch' );
-        }    
-    };
+		}
+	};
+	
+	$.fn.ulSearch = function(methodOrOptions) {
+		if ( methods[methodOrOptions] ) {
+			return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+			// Default to "init"
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.ulSearch' );
+		}    
+	};
 })( jQuery );
